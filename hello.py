408f5df77b632204a1af60a9ee77f24eeff9a672
rishiak007 <79655014+rishiak007@users.noreply.github.com>
2021-06-26 23:14:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(_name_)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World Suriya'
+    return 'Hello World Rishi'
 
 if _name_ == '_main_':
    app.run(host="0.0.0.0", port=80)
